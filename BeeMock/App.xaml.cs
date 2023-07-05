3a72a514df719d4e08c1b39d8bf5653251337e68
Zhan Cao <ohyesboy@gmail.com>
2023-07-05 17:45:30 -0400

    Init

---
+++ BeeMock/App.xaml.cs
@@ -0,0 +1,23 @@
+﻿namespace BeeMock;
+
+public partial class App : Application
+{
+	public App()
+	{
+		InitializeComponent();
+        UserAppTheme = AppTheme.Light;
+        MainPage = new MainPage();
+	}
+
+    protected override Window CreateWindow(IActivationState activationState)
+    {
+        var window =  base.CreateWindow(activationState);
+        if(DeviceInfo.Current.Idiom ==  DeviceIdiom.Desktop && DeviceInfo.Current.Platform == DevicePlatform.WinUI)
+        {
+            window.Width = 450;
+            window.Height = 900;
+        }
+  
+        return window;
+    }
+}
---
+++ BeeMock/Pages/MainPage.xaml.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections.ObjectModel;
+using System.ComponentModel;
+using System.Runtime.CompilerServices;
+
+namespace BeeMock;
+
+public static class Icons
+{
+    public const string Search = "\uE801";
+    public const string Menu = "\uF0C9";
+    public const string Home_Outline = "\uE800";
+    public const string Home = "\uE802";
+    public const string Doc_Text = "\uF0F6";
+    public const string Forumbee = "\uF211";
+    public const string Sliders = "\uF1DE";
+}
+
+public partial class MainPage : ContentPage, INotifyPropertyChanged
+{
+    public string Title { get; set; }
+    public ObservableCollection<Article> Articles { get; set; }
+	public MainPage()
+    {
+        InitializeComponent();
+        Title = "Titles";
+        Articles = new ObservableCollection<Article>()
+        {
+            new Article(){Title="Summer Frenzy", ImgSource="sea.png"},
+            new Article(){Title="Tokyo cold", ImgSource="cold.png"},
+        };
+        this.BindingContext = this;
+
+    }
+}
+public class Article
+{
+    public string Title { get; set; }
+    public string ImgSource { get; set; }
+
+}
+
