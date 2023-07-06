19de56d518308bd988428fdfe16196d9d4b9ff22
Zhan Cao <ohyesboy@gmail.com>
2023-07-06 19:45:34 -0400

    style bind to data

--- BeeMock/Pages/IconButton.xaml.cs
+++ BeeMock/Pages/IconButton.xaml.cs
@@ -1,18 +1,64 @@
+using System.Collections.ObjectModel;
+using System.ComponentModel;
+using System.Runtime.CompilerServices;
+
 namespace BeeMock;
 
 public partial class IconButton : ContentView
 {
 
-	private Color _TextColor;
-	public Color TextColor { get { return _TextColor; } set { if (_TextColor == value) return; _TextColor = value; OnPropertyChanged(); } }
-	private string _Icon;
-	public string Icon { get { return _Icon; } set { if (_Icon == value) return; _Icon = value; OnPropertyChanged(); } }
-	private string _Text;
-	public string Text { get { return _Text; } set { if (_Text == value) return; _Text = value; OnPropertyChanged(); } }
+	public ObservableCollection<IconButtonModel> Buttons { get; set; } = new ObservableCollection<IconButtonModel>();
     public IconButton()
 	{
 		InitializeComponent();
-		this.BindingContext = this;
+		Buttons.Add(new IconButtonModel { Icon = Icons.Doc_Text, Selected = true, Text = "Doc" });
+        Buttons.Add(new IconButtonModel { Icon = Icons.Menu, Text = "Options" });
+		var index = 0;
+		foreach(var b in Buttons)
+		{
+			b.Order = index++;
+		}
+        this.BindingContext = this;
 
 	}
+
+    void TapGestureRecognizer_Tapped(System.Object sender, Microsoft.Maui.Controls.TappedEventArgs e)
+    {
+		
+
+    }
 }
+
+public class IconButtonModel: INotifyPropertyChanged {
+    private string _Icon;
+    public string Icon { get { return _Icon; } set { if (_Icon == value) return; _Icon = value; OnPropertyChanged(); } }
+    private string _Text;
+    public string Text { get { return _Text; } set { if (_Text == value) return; _Text = value; OnPropertyChanged(); } }
+	bool _Selected;
+	public bool Selected { get => _Selected; set { if (_Selected == value) return; _Selected = value; PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(nameof(Selected))); } }
+	int _Order;
+	public int Order { get => _Order; set { if (_Order == value) return; _Order = value; PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(nameof(Order))); } }
+	public event PropertyChangedEventHandler PropertyChanged;
+
+	public void OnPropertyChanged([CallerMemberName] string propertyName = null)
+	{
+		PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+	}
+
+
+}
+
+public class IconButtonTemplateSelector : DataTemplateSelector
+{
+	public DataTemplate Selected { get; set; }
+	public DataTemplate UnSelected { get; set; }
+    protected override DataTemplate OnSelectTemplate(object item, BindableObject container)
+    {
+		var model = item as IconButtonModel;
+		if (model.Selected)
+			return Selected;
+		else
+			return UnSelected;
+    }
+}
+
