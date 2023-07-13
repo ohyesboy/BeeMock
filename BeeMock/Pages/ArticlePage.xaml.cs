3a726d3daa1e106c5f97af96aa131d64bb978f74
Zhan Cao <ohyesboy@gmail.com>
2023-07-13 11:21:26 -0400

    Fix bug

--- BeeMock/Pages/ArticlePage.xaml.cs
+++ BeeMock/Pages/ArticlePage.xaml.cs
@@ -1,6 +1,7 @@
 ﻿using System.Collections.ObjectModel;
 using System.Timers;
 using CommunityToolkit.Mvvm.ComponentModel;
+using Microsoft.Maui.Controls.Shapes;
 using Plugin.Maui.Audio;
 
 namespace BeeMock;
@@ -16,8 +17,8 @@ public partial class ArticlePage : ContentPage
     private System.Timers.Timer aTimer;
 
     public ArticlePage(ArticlePageModel model)
-	{
-		InitializeComponent();
+    {
+        InitializeComponent();
         this.BindingContext = model;
         this.model = model;
         model.ButtonText = "Play";
@@ -31,13 +32,34 @@ public partial class ArticlePage : ContentPage
         var content = sr.ReadToEnd().Split(Environment.NewLine);
         stream.Close();
 
-        foreach(var line in content)
+        foreach (var line in content)
         {
             var parts = line.Split('\t', 2);
             if (parts.Length != 2)
                 continue;
             var span = TimeSpan.Parse(parts[0]);
             model.Segments.Add(new ScriptSegment { Text = parts[1], TimeStamp = span.TotalSeconds });
+        }
+        for (int i = 0; i < model.Segments.Count; i++)
+        {
+            var seg = model.Segments[i];
+            var segIndex = model.Segments.IndexOf(seg);
+
+
+            seg.WordSegs = seg.Text.Split(' ', StringSplitOptions.RemoveEmptyEntries)
+                .Select(x => new WordSegment { Word = x })
+                .ToArray();
+            if (seg.WordSegs.Length == 0)
+                continue;
+            var segTime = seg.TimeStamp / seg.WordSegs.Length;
+            for (int wi = 0; wi < seg.WordSegs.Length; wi++)
+            {
+                double prevSegTime = 0;
+                if (i > 0)
+                    prevSegTime = model.Segments[i-1].TimeStamp;
+                seg.WordSegs[wi].TimeStamp = prevSegTime + segTime * (wi + 1);
+            }
+          
         }
 
     }
@@ -51,7 +73,7 @@ public partial class ArticlePage : ContentPage
     private void SetTimer()
     {
         // Create a timer with a two second interval.
-        aTimer = new System.Timers.Timer(0.2);
+        aTimer = new System.Timers.Timer(100);
         // Hook up the Elapsed event for the timer. 
         aTimer.Elapsed += OnTimedEvent;
         //aTimer.AutoReset = true;
@@ -61,13 +83,20 @@ public partial class ArticlePage : ContentPage
 
     private void OnTimedEvent(Object source, ElapsedEventArgs e)
     {
-        var currnet = model.Segments.FirstOrDefault(x => x.TimeStamp > player.CurrentPosition);
-        if (currnet == null)
-            return;
-        var index = model.Segments.IndexOf(currnet);
-        if (index > 0)
-            model.Segments[index - 1].IsCurrent = false;
-        currnet.IsCurrent = true;
+      
+        //var currnet = model.Segments.FirstOrDefault(x => x.TimeStamp > player.CurrentPosition);
+        //if (currnet == null)
+        //    return;
+        //var index = model.Segments.IndexOf(currnet);
+        //if (index > 0)
+        //    model.Segments[index - 1].IsCurrent = false;
+        //currnet.IsCurrent = true;
+
+        foreach (var word in model.Segments.SelectMany(x=>x.WordSegs))
+        {
+            if (word.TimeStamp < player.CurrentPosition)
+                word.IsCurrent = true;
+        }
     }
     private void Stop()
     {
@@ -101,21 +130,21 @@ public partial class ArticlePage : ContentPage
             player.Pause();
             model.ButtonText = "Play";
         }
-           
+
         else
         {
             player.Play();
             model.ButtonText = "Pause";
         }
-          
-        
+
+
     }
 
 }
 
 
-[QueryProperty(nameof(Id),nameof(Id))]
-public partial class ArticlePageModel: ObservableObject
+[QueryProperty(nameof(Id), nameof(Id))]
+public partial class ArticlePageModel : ObservableObject
 {
     [ObservableProperty]
     int id;
@@ -129,15 +158,32 @@ public partial class ArticlePageModel: ObservableObject
 }
 
 
+public partial class WordSegment : ObservableObject
+{
+
+    public double TimeStamp { get; set; }
+    public string Word { get; set; }
+    [ObservableProperty]
+    bool isCurrent;
+
+    partial void OnIsCurrentChanged(bool value)
+    {
+        if (!value)
+            return;
+        //
+    }
+}
+
 public partial class ScriptSegment : ObservableObject
 {
+    public string Text { get; set; }
+
     [ObservableProperty]
-    string text;
+    WordSegment[] wordSegs;
 
     [ObservableProperty]
     bool isCurrent;
 
-    [ObservableProperty]
-    double timeStamp;
+    public double TimeStamp { get; set; }
 
 }
